6ea19c4cab6735a0ab1ec1973500ae8c653a3e02
Bomi Choi <hibomi97@gmail.com>
2022-06-17 17:44:21 +0900

    Feat: User, Social 어드민 페이지 등록

---
+++ backend/app/user/admin.py
@@ -0,0 +1,52 @@
+from django.contrib import admin
+from . import models
+from django.contrib.auth.admin import UserAdmin
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """Custom User Admin"""
+
+    fieldsets = (
+        ('Personal info', 
+            {'fields': (
+            "username",
+            "password",
+            'nickname',
+            'email', 
+            "is_active",
+            "is_staff",
+            "is_superuser",
+            "is_register",
+            "gender",
+            "age",
+            "address",
+            "profile",
+            )}
+        ),
+    )
+
+    list_filter = UserAdmin.list_filter
+
+    list_display = (
+        "username",
+        "nickname",
+        "email",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        "is_register",
+        "gender",
+        "age",
+        "address",
+        "profile",
+        "created"
+    )
+
+@admin.register(models.Social)
+class SocialAdmin(admin.ModelAdmin):
+
+    """Review Admin Definition"""
+
+    list_display = ("user","kind")
