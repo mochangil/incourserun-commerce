1bc306c68bffcde2de2c33f2bdd21b8ad83c351c
Bomi Choi <hibomi97@gmail.com>
2022-06-20 14:54:06 +0900

    Feat: Product urls.py 수정

--- backend/app/product/urls.py
+++ backend/app/product/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = "Product"
+app_name = "product"
 
 urlpatterns = [
-    path('/', views.ProductListCreateView.as_view()),
+    path('', views.ProductListCreateView.as_view()),
     path('/<int:pk>', views.ProductDetailUpdateDeleteView.as_view()),
     path('/hashtag', views.HashtagListCreateView.as_view()),
     path('/hashtag/<int:pk>', views.HashtagDetailUpdateDeleteView.as_view())
