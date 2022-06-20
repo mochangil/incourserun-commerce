8f439cdf35617cdc09dec759b39e80092af3f769
Bomi Choi <hibomi97@gmail.com>
2022-06-20 17:50:38 +0900

    Feat: Order, OrderProduct 어드민 페이지 등록

--- backend/app/order/admin.py
+++ backend/app/order/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'created',
+        'shipping_name',
+        'shipping_phone',
+        'shipping_zipcode',
+        'shipping_address',
+        'shipping_address_detail',
+        'shipping_request',
+        'shipping_status',
+        'pay_method',
+        'pay_date',
+        'pay_status',
+        'total_amount',
+        'delivery_fee'
+    )
+
+
+@admin.register(models.OrderProduct)
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in models.OrderProduct._meta.get_fields()]
