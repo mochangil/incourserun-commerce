07790e6b677796b5b945cdf78af11fe86b99f355
Bomi Choi <hibomi97@gmail.com>
2022-06-20 17:27:51 +0900

    Feat: Order, OrderProduct 모델 생성

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -17,7 +17,8 @@ LOCAL_APPS = [
     "app.staticfile",
     "app.example.apps.ExampleConfig",
     "app.user.apps.UserConfig",
-    "app.product.apps.ProductConfig"
+    "app.product.apps.ProductConfig",
+    "app.order.apps.OrderConfig"
 ]
 
 THIRD_PARTY_APPS = [
