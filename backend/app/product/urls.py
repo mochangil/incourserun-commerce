356fc199279c9200ae491c2c59f5bf9eade40e9e
Bomi Choi <hibomi97@gmail.com>
2022-06-20 14:31:01 +0900

    Feat: Product, Hashtag CRUD API 추가

---
+++ backend/app/product/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = "Product"
+
+urlpatterns = [
+    path('/', views.ProductListCreateView.as_view()),
+    path('/<int:pk>', views.ProductDetailUpdateDeleteView.as_view()),
+    path('/hashtag', views.HashtagListCreateView.as_view()),
+    path('/hashtag/<int:pk>', views.HashtagDetailUpdateDeleteView.as_view())
+]
