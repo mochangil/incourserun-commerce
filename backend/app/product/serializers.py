a2363edae630cc5031529c784e8a4670ad4969e2
Bomi Choi <hibomi97@gmail.com>
2022-06-21 12:41:31 +0900

    Feat: avg_rating, review_count 필드 추가

--- backend/app/product/serializers.py
+++ backend/app/product/serializers.py
@@ -3,6 +3,9 @@ from .models import Product, Hashtag
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    avg_rating = serializers.FloatField(read_only = True)
+    review_count = serializers.IntegerField(read_only = True)
+
     class Meta:
         model = Product
         fields = '__all__'
