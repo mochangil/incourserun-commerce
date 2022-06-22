edebf585ec9070c09c6737323695f51f42b57748
Bomi Choi <hibomi97@gmail.com>
2022-06-22 11:42:29 +0900

    Feat: Order에 Nested Serializer 추가

--- backend/app/order/serializers.py
+++ backend/app/order/serializers.py
@@ -1,14 +1,38 @@
 from rest_framework import serializers
 from .models import Order, OrderProduct
-
-
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = '__all__'
+from ..product.serializers import ProductSerializer
 
 
 class OrderProductSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+
     class Meta:
         model = OrderProduct
         fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_products = OrderProductSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = (
+            'id',
+            'user',
+            'created',
+            'shipping_name',
+            'shipping_phone',
+            'shipping_zipcode',
+            'shipping_address',
+            'shipping_address_detail',
+            'shipping_request',
+            'shipping_status',
+            'pay_method',
+            'pay_date',
+            'pay_status',
+            'total_amount',
+            'delivery_fee',
+            'order_products'
+        )
+
+
