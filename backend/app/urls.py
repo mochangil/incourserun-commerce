afdd0b4bb757946758c99a3760092e4f31fd8c28
Bomi Choi <hibomi97@gmail.com>
2022-06-23 11:27:30 +0900

    Refactor: API 경로명 복수형으로 변경

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("example", include("app.example.urls")),
-    path("user", include("app.user.urls")),
-    path("product", include("app.product.urls")),
-    path("order", include("app.order.urls")),
-    path('review', include("app.review.urls"))
+    path("users", include("app.user.urls")),
+    path("products", include("app.product.urls")),
+    path("orders", include("app.order.urls")),
+    path('reviews', include("app.review.urls"))
 ]
