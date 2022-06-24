e1ae787758a7b2ad096d6da602d08562b7beed64
Bomi Choi <hibomi97@gmail.com>
2022-06-24 17:31:34 +0900

    Feat: Cart Filterset 추가

--- backend/app/user/filters.py
+++ backend/app/user/filters.py
@@ -1,7 +1,12 @@
 from django_filters import rest_framework as filters
-from .models import User
+from .models import User, Cart
 
 class UserFilter(filters.FilterSet):
     class Meta:
         model = User
         fields = ['email']
+
+class CartFilter(filters.FilterSet):
+    class Meta:
+        model = Cart
+        fields = ['user', 'product']
