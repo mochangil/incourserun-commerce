edc90f3c05c1239291efd3b68fe14c359edbd2e9
Bomi Choi <hibomi97@gmail.com>
2022-06-24 21:54:00 +0900

    Feat: Cart 모델 amount 기본값 추가, UniqueConstraint 설정

--- backend/app/user/models.py
+++ backend/app/user/models.py
@@ -89,8 +89,11 @@ class Social(models.Model):
 class Cart(models.Model):
     user = models.ForeignKey('user.User', related_name="carts", on_delete=models.CASCADE)
     product = models.ForeignKey('product.Product', related_name="carts", on_delete=models.CASCADE)
-    amount = models.IntegerField(verbose_name="수량", validators=[MinValueValidator(1)])
+    amount = models.IntegerField(verbose_name="수량", validators=[MinValueValidator(1)], default=1)
 
     class Meta:
         verbose_name = "장바구니"
         verbose_name_plural = verbose_name
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'product'], name='unique_user_product'),
+        ]
