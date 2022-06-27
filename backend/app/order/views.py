0903cc3eb47818af724bd33f7bc88a30479bd7ad
Bomi Choi <hibomi97@gmail.com>
2022-06-27 16:50:31 +0900

    Feat: Order, OrderProduct Filterset 추가

--- backend/app/order/views.py
+++ backend/app/order/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
+from django_filters import rest_framework as filters
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Order, OrderProduct
 from .serializers import OrderSerializer, OrderProductSerializer
+from .filters import OrderFilter, OrderProductFilter
 
 # Create your views here.
 class OrderListCreateView(ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_class = OrderFilter
 
 
 class OrderDetailUpdateDeleteView(RetrieveUpdateDestroyAPIView):
@@ -17,6 +21,8 @@ class OrderDetailUpdateDeleteView(RetrieveUpdateDestroyAPIView):
 class OrderProductListCreateView(ListCreateAPIView):
     queryset = OrderProduct.objects.all()
     serializer_class = OrderProductSerializer
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_class = OrderProductFilter
 
 
 class OrderProductDetailUpdateDeleteView(RetrieveUpdateDestroyAPIView):
