95d0f5be1c6df015a28c1d457f52cde12793828a
Bomi Choi <hibomi97@gmail.com>
2022-06-29 16:42:23 +0900

    Feat: Review 어드민 페이지에 사진 개수 보기, 리뷰이미지 미리보기 기능 추가

--- backend/app/review/admin.py
+++ backend/app/review/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from . import models
+from django.utils.html import mark_safe
 
 # Register your models here.
 @admin.register(models.Review)
@@ -10,16 +11,25 @@ class ReviewAdmin(admin.ModelAdmin):
         'product',
         'rating',
         'content',
-        'created_at'
+        'count_photos',
+        'created_at',
     )
+
+    def count_photos(eslf, obj):
+        return obj.photos.count()
+    count_photos.short_description = "사진 개수"
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
     list_display = (
         'id',
         'review',
-        'img'
+        'get_img',
     )
+    def get_img(self, obj):
+        url = obj.img.url if obj.img else ""
+        return mark_safe(f'<div> <div><a href="{url}">{obj.img}</a></div> <img width="100px" src="{url}" /> </div>')
+    get_img.short_description = "이미지"
 
 @admin.register(models.Reply)
 class ReplyAdmin(admin.ModelAdmin):
@@ -27,6 +37,6 @@ class ReplyAdmin(admin.ModelAdmin):
         'id',
         'review',
         'content',
-        'created_at'
+        'created_at',
     )
 
