a93ec27b1386ae5f3cd23e73abf43d589816239e
Bomi Choi <hibomi97@gmail.com>
2022-06-30 12:30:46 +0900

    Feat: Review Validation 추가

--- backend/app/review/serializers.py
+++ backend/app/review/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from .models import Review, Photo, Reply
 
 
@@ -18,7 +19,15 @@ class ReviewSerializer(serializers.ModelSerializer):
     photos = PhotoSerializer(many=True, read_only=True)
     reply = ReplySerializer(read_only=True)
     photo_count = serializers.IntegerField(read_only=True)
-    
+
+    def validate(self, attrs):
+        order = attrs['order_product'].order
+        if order.user != self.context['request'].user:
+            raise ValidationError({'user': '주문자와 작성자가 일치하지 않습니다.'})
+        if order.shipping_status != "배송완료":
+            raise ValidationError({'shippingStatus': '배송 완료된 상품만 작성 가능합니다.'})
+        return attrs    
+
     class Meta:
         model = Review
         fields = (
@@ -33,5 +42,3 @@ class ReviewSerializer(serializers.ModelSerializer):
             'reply',
             'photo_count'
         )
-
-    
