f5ce5d1bdb84bc5b400d393626f2442628e45bf7
Bomi Choi <hibomi97@gmail.com>
2022-06-30 11:56:26 +0900

    Chore: Review, Photo, Reply 모델 필드에 verbose_name 추가

--- backend/app/review/models.py
+++ backend/app/review/models.py
@@ -3,9 +3,9 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Review(models.Model):
-    user = models.ForeignKey('user.User', related_name="reviews", on_delete=models.CASCADE)
-    product = models.ForeignKey('product.Product', related_name="reviews", on_delete=models.CASCADE)
-    order_product = models.OneToOneField('order.OrderProduct', related_name="review", on_delete=models.CASCADE)
+    user = models.ForeignKey('user.User', verbose_name="작성자", related_name="reviews", on_delete=models.CASCADE)
+    product = models.ForeignKey('product.Product', verbose_name="상품", related_name="reviews", on_delete=models.CASCADE)
+    order_product = models.OneToOneField('order.OrderProduct', verbose_name="주문-상품", related_name="review", on_delete=models.CASCADE)
     rating = models.IntegerField(verbose_name="별점", validators=[MinValueValidator(1), MaxValueValidator(5)])
     content = models.CharField(verbose_name="내용", max_length = 1000, null=True, blank=True)
     created_at = models.DateTimeField(verbose_name="작성일시", auto_now_add=True)
@@ -19,7 +19,7 @@ class Review(models.Model):
 
 
 class Photo(models.Model):
-    review = models.ForeignKey('review.Review', related_name="photos", on_delete=models.CASCADE)
+    review = models.ForeignKey('review.Review', verbose_name="리뷰", related_name="photos", on_delete=models.CASCADE)
     img = models.ImageField(verbose_name="이미지", upload_to="review")
 
     class Meta:
@@ -31,7 +31,7 @@ class Photo(models.Model):
 
 
 class Reply(models.Model):
-    review = models.OneToOneField('review.Review', related_name="reply", on_delete=models.CASCADE)
+    review = models.OneToOneField('review.Review', verbose_name="리뷰", related_name="reply", on_delete=models.CASCADE)
     content = models.CharField(verbose_name="내용", max_length = 1000)
     created_at = models.DateTimeField(verbose_name="작성일시", auto_now_add=True)
 
