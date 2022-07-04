34ad281dcc281f2e80f58cf2411042f7e91d6ecf
Bomi Choi <hibomi97@gmail.com>
2022-07-04 12:03:19 +0900

    Feat: Product 어드민 페이지에 상품이미지, 상세이미지 미리보기 기능 추가

--- backend/app/product/admin.py
+++ backend/app/product/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from . import models
+from django.utils.html import mark_safe
 
 # Register your models here.
 @admin.register(models.Product)
@@ -11,10 +12,20 @@ class ProductAdmin(admin.ModelAdmin):
         'capacity',
         'price',
         'description',
-        'product_img',
-        'detail_img',
+        'get_product_img',
+        'get_detail_img',
         'created_at',
     )
+    
+    def get_product_img(self, obj):
+        url = obj.product_img.url if obj.product_img else ""
+        return mark_safe(f'<div> <div><a href="{url}">{obj.product_img}</a></div> <img width="100px" src="{url}" /> </div>')
+    get_product_img.short_description = "상품이미지"
+
+    def get_detail_img(self, obj):
+        url = obj.detail_img.url if obj.detail_img else ""
+        return mark_safe(f'<div> <div><a href="{url}">{obj.detail_img}</a></div> <img width="100px" height="200px" src="{url}" /> </div>')
+    get_detail_img.short_description = "상세이미지"
 
 
 @admin.register(models.Hashtag)
