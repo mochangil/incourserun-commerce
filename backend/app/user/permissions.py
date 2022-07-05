206489fb22f824a704d6b0e1e9f95d39cf1d597c
Bomi Choi <hibomi97@gmail.com>
2022-07-05 17:10:51 +0900

    Fix: UserPermission 수정

--- backend/app/user/permissions.py
+++ backend/app/user/permissions.py
@@ -2,8 +2,7 @@ from rest_framework.permissions import BasePermission
 
 class UserPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
-        print(request.user, obj.user)
-        return obj.user == request.user
+        return obj == request.user
 
 class CartPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
