9cb4f95394edd2d2dc72a5f766849f8984fbbd4f
Bomi Choi <hibomi97@gmail.com>
2022-07-05 18:08:15 +0900

    Refactor: UserSocialLoginSerializer 중복 코드 제거

--- backend/app/user/serializers.py
+++ backend/app/user/serializers.py
@@ -45,10 +45,14 @@ class UserSocialLoginSerializer(serializers.Serializer):
             'password': make_password(None)
         })
 
-        if created:
-            # user 데이터 추가
-            user.username = kakao_account['email']
-            user.nickname = kakao_account['profile']['nickname']
+        if created or user.is_active == False:
+             # user 데이터 추가
+            if created: # 새로 가입한 유저인 경우
+                user.username = kakao_account['email']
+                user.nickname = kakao_account['profile']['nickname']
+
+            if user.is_active == False: #탈퇴했던 유저인 경우
+                user.is_active = True
 
             if kakao_account['has_gender']:
                 if kakao_account['gender'] == 'male':
@@ -78,38 +82,6 @@ class UserSocialLoginSerializer(serializers.Serializer):
             user.save()
 
             # Social 정보 저장
-            Social.objects.create(user=user, kind=state)
-        
-        #탈퇴했던 유저인경우
-        elif user.is_active == False:
-            user.is_active = True
-            if kakao_account['has_gender']:
-                if kakao_account['gender'] == 'male':
-                    user.gender = GenderChoices.MALE.value
-                if kakao_account['gender'] == 'female':
-                    user.gender = GenderChoices.FEMALE.value
-
-            if kakao_account['has_age_range']:
-                age = kakao_account['age_range']
-                if age == '10~14' or age == '15~19':
-                    user.age = AgeChoices.TEEN.value
-                elif age == '20~29':
-                    user.age = AgeChoices.TWENTY.value
-                elif age == '30~39':
-                    user.age = AgeChoices.THIRTY.value
-                elif age == '40~49':
-                    user.age = AgeChoices.FORTY.value
-                else:
-                    user.age = AgeChoices.OVER_FIFTY.value
-
-            # 프로필 이미지 저장
-            img_temp = NamedTemporaryFile(delete=True)
-            img_temp.write(urlopen(kakao_account['profile']['profile_image_url']).read())
-            img_temp.flush()
-            user.profile_img.save(f'profile{user.pk}.jpg', File(img_temp))
-
-            user.save()
-            
             Social.objects.create(user=user, kind=state)
 
         refresh = RefreshToken.for_user(user)
