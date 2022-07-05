bdea577e7553bb714bf104463733278c65dbd782
Bomi Choi <hibomi97@gmail.com>
2022-07-05 12:10:43 +0900

    Feat: Review 어드민 페이지 inline 추가, has_reply 필드 추가

--- backend/app/review/admin.py
+++ backend/app/review/admin.py
@@ -2,9 +2,17 @@ from django.contrib import admin
 from . import models
 from django.utils.html import mark_safe
 
-# Register your models here.
+
+class PhotoInline(admin.TabularInline):
+    model = models.Photo
+
+class ReplyInline(admin.TabularInline):
+    model = models.Reply
+
+
 @admin.register(models.Review)
 class ReviewAdmin(admin.ModelAdmin):
+    inlines = (PhotoInline, ReplyInline)
     list_display =  (
         'id',
         'user',
@@ -14,6 +22,7 @@ class ReviewAdmin(admin.ModelAdmin):
         'content',
         'count_photos',
         'has_photo',
+        'has_reply',
         'created_at',
     )
 
@@ -26,6 +35,12 @@ class ReviewAdmin(admin.ModelAdmin):
     has_photo.short_description = "포토리뷰"
     has_photo.boolean = True
 
+    def has_reply(self, obj):
+        return hasattr(obj, 'reply')
+    has_reply.short_description = "답글 등록 여부"
+    has_reply.boolean = True
+
+
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
     list_display = (
@@ -37,6 +52,7 @@ class PhotoAdmin(admin.ModelAdmin):
         url = obj.img.url if obj.img else ""
         return mark_safe(f'<div> <div><a href="{url}">{obj.img}</a></div> <img width="100px" src="{url}" /> </div>')
     get_img.short_description = "이미지"
+
 
 @admin.register(models.Reply)
 class ReplyAdmin(admin.ModelAdmin):
