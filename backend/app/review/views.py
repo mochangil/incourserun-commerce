a45d0038d2fe5e0c3bc755e0a17a56d0d43158e7
Bomi Choi <hibomi97@gmail.com>
2022-07-08 11:15:36 +0900

    Review의 has_photo 필드 모델 안으로 이동

--- backend/app/review/views.py
+++ backend/app/review/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from django.db.models import OuterRef, Exists
-from django.db.models.functions import Coalesce
 from django_filters import rest_framework as filters
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import ListCreateAPIView
-from .models import Review, Photo
+from .models import Review
 from .serializers import ReviewSerializer
 from .filters import ReviewFilter
 from .paginations import ReviewPagination
@@ -13,10 +11,7 @@ from ..common.permissions import IsOwner, IsStaff
 
 # Create your views here.
 class ReviewListCreateView(ListCreateAPIView):
-    photo_count_subquery = Photo.objects.filter(review=OuterRef('id')).values('review')
-    queryset = Review.objects.annotate(
-        has_photo = Exists(photo_count_subquery)
-    )
+    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     filter_backends = [filters.DjangoFilterBackend, OrderingFilter]
     filterset_class = ReviewFilter
