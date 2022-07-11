242dc18aa21c2b372b54bbb86046eafc9aac0d89
Bomi Choi <hibomi97@gmail.com>
2022-07-11 17:27:10 +0900

    Feat: Order API PUT/PATCH/DELETE 메소드 제거

--- backend/app/order/serializers.py
+++ backend/app/order/serializers.py
@@ -16,39 +16,6 @@ class OrderProductSerializer(serializers.ModelSerializer):
         fields = ('id', 'product', 'quantity', 'price', 'shipping_status', 'is_cancelled', 'has_review')
 
 
-class OrderProductUpdateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OrderProduct
-        fields = ('is_cancelled',)
-
-    def update(self, instance, validated_data):
-        if instance.shipping_status != "결제완료":
-            raise ValidationError({'status':'상품이 결제완료 상태일 때만 변경 가능합니다.'})
-        instance.is_cancelled = validated_data['is_cancelled']
-
-        # 총 주문금액 업데이트
-        order = instance.order
-        if instance.is_cancelled:
-            # 주문취소
-            order.total_price -= instance.price * instance.quantity
-            if order.total_price == 0:
-                order.is_cancelled = True
-                order.delivery_fee = 0
-            elif order.total_price < 30000:
-                order.delivery_fee = 3000
-        else:
-            # 주문취소 철회
-            order.is_cancelled = False
-            order.total_price += instance.price * instance.quantity
-            if order.total_price < 30000:
-                order.delivery_fee = 3000
-            else:
-                order.delivery_fee = 0
-        instance.save()
-        order.save()
-        return instance
-
-
 class OrderSerializer(serializers.ModelSerializer):
     User = get_user_model()
     order_products = OrderProductSerializer(many=True)
@@ -89,18 +56,6 @@ class OrderSerializer(serializers.ModelSerializer):
         for order_product in order_products:
             OrderProduct.objects.create(order=order, **order_product)
         return order
-
-class OrderUpdateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = ('is_cancelled',)
-
-    def update(self, instance, validated_data):
-        print(instance)
-        if instance.shipping_status != "결제완료":
-            raise ValidationError({'status':'주문이 결제완료 상태일 때만 변경 가능합니다.'})
-        instance.is_cancelled = validated_data['is_cancelled']
-        return instance
         
 
 class CancelSerializer(serializers.Serializer):
--- backend/app/order/urls.py
+++ backend/app/order/urls.py
@@ -5,8 +5,8 @@ app_name = "order"
 
 urlpatterns = [
     path('', views.OrderListCreateView.as_view()),
-    path('/<int:pk>', views.OrderDetailUpdateDeleteView.as_view()),
-    path('/order-products/<int:pk>', views.OrderProductDetailUpdateView.as_view()),
+    path('/<int:pk>', views.OrderDetailView.as_view()),
+    path('/order-products/<int:pk>', views.OrderProductDetailView.as_view()),
     path('/payment/complete',views.OrderPaymentView.as_view()),
     path('/payment/cancel', views.CancelCreateView.as_view()),
     path('/payment/complete',views.OrderPaymentView.as_view()),
