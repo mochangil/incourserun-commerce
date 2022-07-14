e1d83504005458ed89a0884ea347cfb82368d042
Bomi Choi <hibomi97@gmail.com>
2022-07-14 17:11:48 +0900

    Fix: DB USER, PASSWORD 직접 입력

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -43,9 +43,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "commerce",
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "USER": 'root',
+        "PASSWORD": 'incourserun',
         "HOST": "commerce.cujsvjlde9dh.ap-northeast-2.rds.amazonaws.com",
         "PORT": "5432",
     }
 }
+print(DATABASES)
